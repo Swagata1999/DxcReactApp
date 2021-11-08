b248b4e0ec419f81be3767d4e252ae2a5bbfb4a9
Swagata1999 <swagatadas1999@gmail.com>
2021-11-08 14:05:35 +0530

    Initial Commit

---
+++ src/Component/Component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Component.css';
+
+export default class Component extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div className="Component">
+			</div>
+		)
+	}
+}
