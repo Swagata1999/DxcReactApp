54fbace375edbd8256da03b02f8c750f67c1048c
Swagata1999 <swagatadas1999@gmail.com>
2021-11-12 09:59:32 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,21 @@ import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+
+import './appStyles.css'
+import styles from './appStyles.module.css'
+import Form from './components/Form';
+import Fragments from './components/Fragments';
+import NewTable from './components/NewTable';
+import PureComp from './components/PureComp';
+import Parentcomp from './components/Parentcomp';
+
+ 
  
 
 function App() {
---
+++ src/components/Parentcomp.js
@@ -0,0 +1,36 @@
+import React, { Component,PureComponent } from 'react'
+import MemoComp from './MemoComp';
+import PureComp from './PureComp';
+import RegularComp from './RegularComp';
+
+ class Parentcomp extends PureComponent {
+constructor(props) {
+    super(props)
+
+    this.state = {
+         name: 'abdul-parentcompnew'
+    }
+}
+
+    componentDidMount(){
+        setInterval(() => {
+            this.setState({
+                name: 'abdul-parentcomp-memo'
+            })
+        }, 2000);
+    }
+
+    render() {
+        console.log('******************* parent component************************')
+        return (
+            <div>
+                Parent component
+                <MemoComp name = {this.state.name}/>
+               { /* <RegularComp name ={this.state.name}></RegularComp>
+        <PureComp name = {this.state.name}></PureComp> */}
+            </div>
+        )
+    }
+}
+
+export default Parentcomp
